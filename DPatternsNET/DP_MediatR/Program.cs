94cc8a48ee97d3d7f9160af2112a1eb3f7cdfec8
shaqablanca <a.sameduz@gmail.com>
2023-11-24 21:58:18 +0100

    MediatR Desing Pattern

---
+++ DPatternsNET/DP_MediatR/Program.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+// Define a simple request and response
+public class Ping : IRequest<string>
+{
+}
+
+// Define a handler for the request
+public class PingHandler : IRequestHandler<Ping, string>
+{
+    public Task<string> Handle(Ping request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult("Pong");
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Setup dependency injection
+        var serviceProvider = new ServiceCollection()
+            .AddMediatR(typeof(Program))
+            .BuildServiceProvider();
+
+        // Get Mediator instance from the service provider
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        // Send a request and get the response
+        var response = await mediator.Send(new Ping());
+
+        // Display the response
+        Console.WriteLine(response);
+    }
+}
